da8daa3efdff77b5874ecdb026387bafff355ca4
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2025-06-04 13:15:52 +0900

    バックエンド側の認証機能作成

---
+++ api/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    // ユーザー登録
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()
+            ], 401);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User created successfully',
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+
+    // ログイン
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User logged in successfully',
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User logged out successfully',
+        ]);
+    }
+
+    public function user(Request $request){
+        return response()->json([
+            'success' => true,
+            'user' => $request->user(),
+        ]);
+    }
+}
